1fad98349110060708d25777ec740ab78d083afa
Bhaskar-Matta <bhaskarmatta77@gmail.com>
2025-02-17 20:48:04 +0530

    Update Python_Code_Reviewer.py

--- Python_Code_Reviewer.py
+++ Python_Code_Reviewer.py
@@ -1,10 +1,8 @@
 import streamlit as st
 import os
 import google.generativeai as genai
-f=open(r"C:\Users\Bhaskar Matta\OneDrive\Desktop\key.txt")
-key=f.read()
-print(key)
-genai.configure(api_key=key)
+API_KEY = st.secrets["API_KEY"]  
+genai.configure(api_key=API_KEY)
 sys_prompt = """You are an AI Code Reviewer.
 Your task is to review code for correctness, efficiency, readability, and best practices.
 Provide constructive feedback, suggest improvements, and highlight potential issues such as bugs, security vulnerabilities, and performance bottlenecks.
